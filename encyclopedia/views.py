5a71294c9e08560bcbdc757671a912fb7599010c
HuuChung98 <balechung93@gmail.com>
2023-10-23 14:23:08 +0700

    complete Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,5 @@
+import markdown2
+
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +10,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def content(request, name):
+
+    entry = util.get_entry(name)
+    if entry:
+        entry = markdown2.markdown(entry)
+    else: 
+        return render(request, "encyclopedia/notfound.html", {
+            "error": name
+        })
+
+    return render(request, "encyclopedia/content.html", {
+        "entry": entry
+    })
