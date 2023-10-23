35bf66caaf0faa8ae29cde8f23ff6a5f8bb5491e
HuuChung98 <balechung93@gmail.com>
2023-10-23 15:41:41 +0700

    Update page Index.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+# app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:name>", views.content, name="content")
+    path("wiki/<str:entry>", views.content, name="content")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,16 +10,23 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def content(request, name):
+def content(request, entry):
 
-    entry = util.get_entry(name)
+    entry = util.get_entry(entry)
     if entry:
         entry = markdown2.markdown(entry)
     else: 
         return render(request, "encyclopedia/notfound.html", {
-            "error": name
+            "error": entry
         })
 
     return render(request, "encyclopedia/content.html", {
         "entry": entry
     })
+
+# def direct_view(request):
+
+#     entry = request.GET.get('entry')
+#     return render(request, "encyclopedia/content.html", {
+#         "entry": entry
+#     })
